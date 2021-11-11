82624b30ca70583894f337c04bf6fe1009c04e64
KSAhh <66674138+KSAhh@users.noreply.github.com>
2021-11-11 20:49:07 +0900

    html 연결

--- jangbwaguni/orderrequest/views.py
+++ jangbwaguni/orderrequest/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def d_order_cus(request):
+    return render(request, 'order_cus.html')
+
+def d_rider_list(request):
+    return render(request, 'rider_list.html')

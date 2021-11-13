1e0ba6fedff9c43195b5dcab2d2c3070c6ce9ee9
patrickkkim <patkim97@gmail.com>
2021-11-13 22:54:33 +0900

    urls 통합

--- jangbwaguni/jangbwaguni/urls.py
+++ jangbwaguni/jangbwaguni/urls.py
@@ -20,5 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('main/', include('main.urls')),
     path('delivery/', include('delivery.urls')),
+    # path('orderrequest/', include('orderrequest.urls')),
+    # path('member/', include('member.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

086976348b98268e912818ae4ab87165fae7c0fb
KSAhh <66674138+KSAhh@users.noreply.github.com>
2021-11-15 14:39:10 +0900

    order_rider 페이지 대략완성

--- jangbwaguni/jangbwaguni/settings.py
+++ jangbwaguni/jangbwaguni/settings.py
@@ -144,6 +144,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # 사용자가 업로드한 파일
 
 STATICFILES_DIRS = [
     Path(BASE_DIR, 'delivery', 'static'),
+    Path(BASE_DIR, 'orderrequest', 'static'),
 ]
 
 # Default primary key field type

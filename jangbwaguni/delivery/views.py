afa137bd92b13780c2726d5920149b34fefcde75
윤경 <57247474+YoonkyungH@users.noreply.github.com>
2021-11-16 23:09:01 +0900

    [delivery] 라이더 등록 수정

--- jangbwaguni/delivery/views.py
+++ jangbwaguni/delivery/views.py
@@ -8,13 +8,27 @@ from django.shortcuts import get_object_or_404
 from .models import Rider, orders
 from django.core.serializers import serialize
 
+from django.views.decorators.csrf import csrf_exempt
+
+# TEST
+from django.core.serializers import serialize
+from rest_framework import serializers
+
 # Create your views here.
 def order_confirm_view(request):
     return render(request, 'delivery/order_confirm.html', {})
 
 # http://127.0.0.1:8000/delivery/register/ -> POST로
 # {"rider_id": "", "rider_pw": "", "rider_name": "", "rider_intro": "", "min_delivery_amount": } 형식으로 send
+@csrf_exempt
 def register_rider_view(request):   # 라이더 등록
+    if request.method == 'GET':
+        rider_list = Rider.objects.all()
+        data = json.loads(serialize('json', rider_list))
+        # return JsonResponse({'rider_lsit': data})
+        return render(request, 'delivery/register_rider.html', {})
+
+
     if request.method == 'POST':
         if request.META['CONTENT_TYPE'] == 'application/json':
             request = json.loads(request.body)
@@ -23,14 +37,20 @@ def register_rider_view(request):   # 라이더 등록
                 rider_pw = request['rider_pw'],
                 rider_name = request['rider_name'],
                 min_delivery_amount=request['min_delivery_amount'],
+                rider_intro = request['rider_intro'],
+                recommended_person=request['recommended_person'],
+                rider_area = request.get('rider_area'),
+                bankbook = request['bankbook'],
+                license = request['license'],
 
-                # 아래 필드들 어떻게 받을지.
-                # rider_intro = request['rider_intro'],
-                # rider_area = request['rider_area'],
+
+                # multiselectfield(여러개 값을 받야아 하는) 체크박스 값이 잘 post되는지 테스트 필요
+                # rider_vehicle = request.POST.getlist('input태그 id값')
+                # rider_vehicle = request.POST.getlist('radio')
+                # rider_vehicle = request.POST.getlist('input태그 name값[]')
+
+
                 # rider_vehicle = request['rider_vehicle'],
-                # bankbook = request['bankbook'],
-                # license = request['license'],
-                # recommended_person = request['recommended_person']
             )
         else:
             rider = Rider(
@@ -38,17 +58,19 @@ def register_rider_view(request):   # 라이더 등록
                 rider_pw=request.POST['rider_pw'],
                 rider_name=request.POST['rider_name'],
                 min_delivery_amount=request.POST['min_delivery_amount'],
+                rider_intro = request.POST['rider_intro'],
+                recommended_person=request.POST['recommended_person'],
+                rider_area=request.POST['rider_area'],
+                bankbook=request.POST['bankbook'],
+                license=request.POST['license'],
 
-                # rider_intro=request.POST['rider_intro'],
-                # rider_area=request.POST['rider_area'],
+
                 # rider_vehicle=request.POST['rider_vehicle'],
-                # bankbook=request.POST['bankbook'],
-                # license=request.POST['license'],
-                # recommended_person=request.POST['recommended_person']
             )
         rider.save()
-        return HttpResponse(status=200)
-    return render(request, 'delivery/register_rider.html', {})
+        # return HttpResponse(status=200)
+        return render(request, 'delivery/register_rider.html', {})
+
 
 def order_list_view(request):
     if request.method == 'GET': # 주문 목록

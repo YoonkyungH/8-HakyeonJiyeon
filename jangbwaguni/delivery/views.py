ff18495fde1e0cb4d017b5a6ee2852b30773064c
윤경 <57247474+YoonkyungH@users.noreply.github.com>
2021-11-16 11:40:27 +0900

    [delivery] 주문 목록, 주문하기

--- jangbwaguni/delivery/views.py
+++ jangbwaguni/delivery/views.py
@@ -5,7 +5,7 @@ import json
 from django.views import View
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404
-from .models import Rider
+from .models import Rider, orders
 from django.core.serializers import serialize
 
 # Create your views here.
@@ -14,7 +14,7 @@ def order_confirm_view(request):
 
 # http://127.0.0.1:8000/delivery/register/ -> POST로
 # {"rider_id": "", "rider_pw": "", "rider_name": "", "rider_intro": "", "min_delivery_amount": } 형식으로 send
-def register_rider_view(request):
+def register_rider_view(request):   # 라이더 등록
     if request.method == 'POST':
         if request.META['CONTENT_TYPE'] == 'application/json':
             request = json.loads(request.body)
@@ -51,4 +51,29 @@ def register_rider_view(request):
     return render(request, 'delivery/register_rider.html', {})
 
 def order_list_view(request):
-    return render(request, 'delivery/order_list.html', {})
+    if request.method == 'GET': # 주문 목록
+        order_list = orders.objects.all()
+        data = json.loads(serialize('json', order_list))
+        return JsonResponse({'order_list': data})
+
+    if request.method == 'POST':    # 주문하기
+        if request.META['CONTENT_TYPE'] == 'application/json':
+            request = json.loads(request.body)
+            order_list = orders(
+                cus_name = request['cus_name'],
+                cus_address = request['cus_address'],
+                # order_product
+                cus_call = request['cus_call'],
+                order_message = request['order_message'],
+            )
+        else:
+            order_list = orders(
+                cus_name = request.POST['cus_name'],
+                cus_address = request['cus_address'],
+                # order_product
+                cus_call = request['cus_call'],
+                order_message = request['order_message'],
+            )
+        order_list.save()
+        return HttpResponse(status=200)
+        # return render(request, 'delivery/order_list.html', {})

dcba61d1fb90e95df6257ae7db660f9c7fa12ead
윤경 <57247474+YoonkyungH@users.noreply.github.com>
2021-11-18 21:48:03 +0900

    .

--- jangbwaguni/delivery/urls.py
+++ jangbwaguni/delivery/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('confirm/', views.order_confirm_view, name="order_confirm"),
+    path('confirm/<int:pk>', views.order_confirm_view, name="order_confirm"),
     path('register/', views.register_rider_view, name="register_rider"),
     path('list/', views.order_list_view, name="order_list"),
     # path('list/<int:pk>/', views.order_list_view, name="order_list"),
--- jangbwaguni/member/admin.py
+++ jangbwaguni/member/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import User
-admin.site.register(User)
+from .models import Customer
+admin.site.register(Customer)
 
 # class UserAdmin(admin.ModelAdmin):
 #     list_display = ('cus_id', )

7a102723b540c8c1cb7483660d356b45363916d2
Patrick Kim <patkim97@gmail.com>
2021-11-18 23:31:37 +0900

    Sooah conflict solved (2) :

--- jangbwaguni/member/urls.py
+++ jangbwaguni/member/urls.py
@@ -5,17 +5,6 @@ from . import views
 
 app_name = 'member'
 urlpatterns = [
-<<<<<<< HEAD
-    path('', views.IndexView.as_view(), name='index'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name="logout"),
-    path('signup/', views.signup, name='signup'),
-    path('list/', views.d_mypage_orderlist, name='d_mypage_orderlist'),
-    path('reviewrider/', views.review_rider_view, name="review_rider"),
-    path('reviewcustomer/', views.review_cus_view, name="review_customer"),
-    path('mypage/', views.mypage_view, name="mypage"),
-]
-=======
     path('list/', views.d_mypage_orderlist, name='d_mypage_orderlist'), 
     path('index/', views.IndexView.as_view(), name='index'),
     path('login/', views.login, name="login"), # 임의
@@ -25,4 +14,3 @@ urlpatterns = [
     path('reviewcustomer/', views.review_cus_view, name="review_customer"),
     path('mypage/', views.mypage_view, name="mypage"),
 ]
->>>>>>> sooah

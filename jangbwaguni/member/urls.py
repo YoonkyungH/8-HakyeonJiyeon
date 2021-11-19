c91ef89041af4fb803d09ab006cb7f1771366c47
Patrick Kim <patkim97@gmail.com>
2021-11-20 05:50:27 +0900

    오류수정

--- jangbwaguni/member/urls.py
+++ jangbwaguni/member/urls.py
@@ -17,8 +17,6 @@ urlpatterns = [
     path('reviewrider/<int:rider_id>/', views.review_rider_view, name="review_rider"),
     path('reviewcustomer/<int:cus_id>/', views.review_cus_view, name="review_customer"),
     path('reviewcustomer/<int:cus_id>/<int:score>', views.review_cus_view, name="review_customer_update"),
-    path('mypage/', views.mypage_view, name="mypage"),
     # path('register/', delivery.views.register_rider_view, name="register_rider"),
-
 ]
 

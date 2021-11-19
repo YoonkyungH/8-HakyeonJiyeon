9a61c2a80b33089fe2cd4704027c1691985c7f04
Patrick Kim <patkim97@gmail.com>
2021-11-20 04:58:07 +0900

    member 관련 수정

--- jangbwaguni/member/urls.py
+++ jangbwaguni/member/urls.py
@@ -10,8 +10,9 @@ urlpatterns = [
     path('login/', views.login, name="login"), # 임의
     path('signup/', views.signup, name='signup'), # 임의
     path('logout/', views.logout, name='logout'),
-    path('reviewrider/', views.review_rider_view, name="review_rider"),
-    path('reviewcustomer/', views.review_cus_view, name="review_customer"),
+    path('reviewrider/<int:rider_id>/', views.review_rider_view, name="review_rider"),
+    path('reviewcustomer/<int:cus_id>/', views.review_cus_view, name="review_customer"),
+    path('reviewcustomer/<int:cus_id>/<int:score>', views.review_cus_view, name="review_customer_update"),
     path('mypage/', views.mypage_view, name="mypage"),
 ]
 
